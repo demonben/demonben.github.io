fea32fd547e5e5b5a514a1a32aceab4440f5a968
demonben <71938599+demonben@users.noreply.github.com>
2020-10-03 04:59:26 -0700

    Update portfolio.js

--- portfolio.js
+++ portfolio.js
@@ -9,3 +9,36 @@ fetch(GITHUB_URL)
     profileImage.src = data.avatar_url;
     console.log(data);
   });
+
+  fetch(GITHUB_URL)
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function() {
+    const profileName = document.getElementById("my-name");
+    profileName.src = data.profile_user_id;
+    console.log();
+  });
+
+
+  const { styler, spring, listen, pointer, value } = window.popmotion;
+
+const ball = document.querySelector(".card");
+const divStyler = styler(ball);
+const ballXY = value({ x: 0, y: 0 }, divStyler.set);
+
+listen(ball, "mousedown touchstart").start(e => {
+e.preventDefault();
+pointer(ballXY.get()).start(ballXY);
+});
+
+listen(document, "mouseup touchend").start(() => {
+spring({
+from: ballXY.get(),
+velocity: ballXY.getVelocity(),
+to: { x: 0, y: 0 },
+stiffness: 200
+// mass: 1,
+// damping: 10
+}).start(ballXY);
+});
