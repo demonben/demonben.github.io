531a58747bf1dc77decbbe7068f4c873fa12206e
demonben <71938599+demonben@users.noreply.github.com>
2020-10-03 06:19:32 -0700

    Update checkform.js

--- checkform.js
+++ checkform.js
@@ -1,5 +1,5 @@
 const checkLength = function(evt) {
-  if (phoneField.value.length > 5 && dddField.value.length > 5 && mailField.value.length > 5 && requests.value.length > 5) {
+  if (phoneField.value.length > 5 && dddField.value.length > 5 && mailField.value.length > 5 && requests.value.length > 5 && select.value.length > 2) {
   	button.removeAttribute('disabled')
   }
 }
@@ -9,11 +9,13 @@ const phoneField = document.querySelector('input[name="phone"]')
 const dddField = document.querySelector('input[name="ddd"]')
 const mailField = document.querySelector('input[name="mail"]')
 const requests = document.querySelector('textarea[name="request"]')
+const select = document.querySelector('select[name="kittens"]')
 const button = document.querySelector('button')
 phoneField.addEventListener('keyup', checkLength)
 dddField.addEventListener('keyup', checkLength)
 mailField.addEventListener('keyup', checkLength)
 requests.addEventListener('keyup', checkLength)
+select.addEventListener('keyup', checkLength)
 
 
 
@@ -40,5 +42,3 @@ form.addEventListener('submit', function (event) {
   console.log('Do you like little kittens??? ', where.value)
   console.log('Special requests: ', message.value)
   })
-
-   
